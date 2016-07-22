dd03ca2008654d80c8346918e0440d54e125b894
alex-nobre <alpnobre@gmail.com>
2016-07-22 21:00:11 +0200

    Adapted analysis for separated segments (TF/ERP) and organized plots.

--- Alpha data preparation.R
+++ Alpha data preparation.R
@@ -27,10 +27,10 @@ library(knitr)
 
 # 1. Import list of files
 # Import file names in working directory
-sqr1.alpha.files <- list.files('./Data/Data_BVA', pattern = "1_Alpha ROI Sqr_FFTBandExport")
-sqr2.alpha.files <- list.files('./Data/Data_BVA', pattern = "2_Alpha ROI Sqr_FFTBandExport")
-rand1.alpha.files <- list.files('./Data/Data_BVA', pattern = "1_Alpha ROI Rand_FFTBandExport")
-rand2.alpha.files <- list.files('./Data/Data_BVA', pattern = "2_Alpha ROI Rand_FFTBandExport")
+sqr1.alpha.files <- list.files('./Data/Data_BVA', pattern = "1_Alpha ROI Sqr_FFTBandExport_pre")
+sqr2.alpha.files <- list.files('./Data/Data_BVA', pattern = "2_Alpha ROI Sqr_FFTBandExport_pre")
+rand1.alpha.files <- list.files('./Data/Data_BVA', pattern = "1_Alpha ROI Rand_FFTBandExport_pre")
+rand2.alpha.files <- list.files('./Data/Data_BVA', pattern = "2_Alpha ROI Rand_FFTBandExport_pre")
 # Concatenate all lists in a single list
 list_fnames <- c(sqr1.alpha.files, sqr2.alpha.files, rand1.alpha.files, 
                  rand2.alpha.files)
@@ -64,7 +64,7 @@ rep_data_long2[rep_data_long2$configuration == 'rand' &
                  rep_data_long2$session == 1,]$alpha <- rand1.mean.alpha
 rep_data_long2[rep_data_long2$configuration == 'rand' & 
                  rep_data_long2$session == 2,]$alpha <- rand2.mean.alpha
-
+# Compute log
 rep_data_long2$log.alpha <- log(rep_data_long2$alpha)
 
 # 6. Add to wide data frame
@@ -73,26 +73,73 @@ rep_data4$alpha.sqr.2 <- sqr2.mean.alpha
 rep_data4$alpha.rand.1 <- rand1.mean.alpha
 rep_data4$alpha.rand.2 <- rand2.mean.alpha
 
+# Compute log
+rep_data4$log.alpha.sqr.1 <- log(rep_data4$alpha.sqr.1)
+rep_data4$log.alpha.sqr.2 <- log(rep_data4$alpha.sqr.2)
+rep_data4$log.alpha.rand.1 <- log(rep_data4$alpha.rand.1)
+rep_data4$log.alpha.rand.2 <- log(rep_data4$alpha.rand.2)
+
+# 7. T-tests for alpha mean power - aware x unaware
+# New grouping
+# alpha
+t.test(rep_data_long2[group == "aware",]$alpha, 
+       rep_data_long2[group == "unaware",]$alpha)
+# log alpha
 t.test(rep_data_long2[group == "aware",]$log.alpha, 
        rep_data_long2[group == "unaware",]$log.alpha)
-
+# effect sizes
 cohen.d(rep_data_long2[group.original == "aware",]$log.alpha, 
         rep_data_long2[group.original == "unaware",]$log.alpha)
 
+# 8. Plots
+# Alpha, new grouping
 alpha.line <- ggplot(rep_data_long2, aes(x = group, y = alpha, 
                                        colour = configuration)) + 
   stat_summary(fun.y = mean, geom = "point") + 
   stat_summary(fun.y = mean, geom = "line", aes(group = configuration)) + 
   stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = 0.2) +
   facet_grid(.~session) +
-  labs(title = "Alpha mean power with exclusion", x = "new group", y = "Alpha mean", 
+  labs(title = "Alpha mean power", x = "new group", y = "Alpha mean", 
        colour = "configuration")
 alpha.line
 
-rep_data_long2 <- rep_data_long2[-which(rep_data_long2$Subject == 33),]
+# Alpha, original grouping
+alpha.line <- ggplot(rep_data_long2, aes(x = group.original, y = alpha, 
+                                         colour = configuration)) + 
+  stat_summary(fun.y = mean, geom = "point") + 
+  stat_summary(fun.y = mean, geom = "line", aes(group = configuration)) + 
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = 0.2) +
+  facet_grid(.~session) +
+  labs(title = "Alpha mean power", x = "original group", y = "Alpha mean", 
+       colour = "configuration")
+alpha.line
 
-plot(1:length(rep_data_long2$Subject), rep_data_long2$alpha)
+# Log alpha, new grouping
+log.alpha.line <- ggplot(rep_data_long2, aes(x = group, y = log.alpha, 
+                                             colour = configuration)) + 
+  stat_summary(fun.y = mean, geom = "point") + 
+  stat_summary(fun.y = mean, geom = "line", aes(group = configuration)) + 
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = 0.2) +
+  facet_grid(.~session) +
+  labs(title = "log.alpha mean power new group", x = "new group", 
+       y = "log.alpha mean", 
+       colour = "configuration")
+log.alpha.line
 
+# Log alpha, original grouping
+log.alpha.line <- ggplot(rep_data_long2, aes(x = group.original, y = log.alpha, 
+                                             colour = configuration)) + 
+  stat_summary(fun.y = mean, geom = "point") + 
+  stat_summary(fun.y = mean, geom = "line", aes(group = configuration)) + 
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = 0.2) +
+  facet_grid(.~session) +
+  labs(title = "log.alpha mean power original group", x = "original group", 
+       y = "log.alpha mean", 
+       colour = "configuration")
+log.alpha.line
+
+# Scatterplots
+# Alpha
 defaults <- par()
 par(mfrow = c(2, 2))
 pdf()
@@ -103,4 +150,14 @@ plot(rep_data4$Subject, rep_data4$alpha.sqr.2, col = 3, pch = 16,
 plot(rep_data4$Subject, rep_data4$alpha.rand.1, col = 4, pch = 16,
      main = "Random, session 1", xlab = 'Subject', ylab = "mean alpha power")
 plot(rep_data4$Subject, rep_data4$alpha.rand.2, col = 6, pch = 16,
+     main = "Random, session 2", xlab = 'Subject', ylab = "mean alpha power")
+
+# Log alpha
+plot(rep_data4$Subject, rep_data4$log.alpha.sqr.1, col = 2, pch = 16, 
+     main = "Square, session 1", xlab = 'Subject', ylab = "mean alpha power")
+plot(rep_data4$Subject, rep_data4$log.alpha.sqr.2, col = 3, pch = 16,
+     main = "Square, session 2", xlab = 'Subject', ylab = "mean alpha power")
+plot(rep_data4$Subject, rep_data4$log.alpha.rand.1, col = 4, pch = 16,
+     main = "Random, session 1", xlab = 'Subject', ylab = "mean alpha power")
+plot(rep_data4$Subject, rep_data4$log.alpha.rand.2, col = 6, pch = 16,
      main = "Random, session 2", xlab = 'Subject', ylab = "mean alpha power")
